0d3f40198e99b2bc93f53983aec47b2ef23e730a
root <root@js.nfweb.ca>
2021-05-22 09:32:03 +0000

    clean

--- swarm/lib/volumes.sh
+++ swarm/lib/volumes.sh
@@ -114,23 +114,6 @@ mount_gluster_volume() {
 
 }
 
-
-# # Returns /mnt/node/volume_name unless BRICK_DIR env is set
-# get_brick_dir() {
-#   local node="${1}"
-#   if defined $BRICK_DIR; then
-#     echo $BRICK_DIR
-#   else
-#     echo "/mnt/${node}/${VOLUME_NAME}"
-#   fi
-# }
-#
-# # abc:/mnt/abc/data-gfs
-# get_brick() {
-#   defined $1 || return  
-#   echo "${1}:$(get_brick_dir $1)"
-# }
-
 # Check if volume exists, pass volume name
 undefined_gluster_volume() {
   undefined $1 && return 0  
@@ -153,9 +136,3 @@ unmounted_gluster_volume() {
   undefined "$(df | grep localhost:/$volume)" && return 0
   return 1
 }
-
-# # Get location of DO's mounted volume
-# get_disk() {
-#   defined $1 || return  
-#   echo "/dev/disk/by-id/scsi-0DO_Volume_${1}"
-# }
