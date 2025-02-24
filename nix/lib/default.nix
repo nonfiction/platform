012caab51eb56c29a26ce75cf825521286562c21
Jon Suderman <jon@suderman.net>
2025-02-24 12:46:33 -0700

    process-compose

--- nix/lib/default.nix
+++ nix/lib/default.nix
@@ -99,9 +99,7 @@ in rec {
     text = ''
       mkdir -p ${expand config.dataDir} ${expand config.wp.uploadsDir}
       ln -sf ${yaml} ./process-compose.yaml
-      process-compose -D > /dev/null 2>&1 & disown
-      echo ready
-      exit 0
+      bash -c 'process-compose -D > /dev/null 2>&1 & disown'
     '';
   in { inherit text; };
 
